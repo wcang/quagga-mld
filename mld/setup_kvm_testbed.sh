d11e1e234fae3591d8a8facc3bd2f263123d84ca
Ang Way Chuang <wcang79@gmail.com>
2013-04-08 21:57:00 +0800

    code to setup kvm testbed

---
+++ mld/setup_kvm_testbed.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#script to setup 2 kvm virtual machine to perform quagga mld testing
+
+start() {
+#	service firewalld stop
+	tunctl -u wcang -t tap_mld1
+	tunctl -u wcang -t tap_mld2
+	ifconfig tap_mld1 up
+	ifconfig tap_mld2 up
+	virsh start mld1
+#	virsh start mld2
+}
+
+stop() {
+#	virsh shutdown mld2
+	virsh shutdown mld1
+	tunctl -d tap_mld1
+	tunctl -d tap_mld2
+	#service firewalld start
+}
+
+case "$1" in
+	start)
+		start
+		;;
+	stop)
+		stop
+		;;
+	restart)
+		stop
+		start
+		;;
+	*)
+		echo "Usage $0 start|stop|restart"
+		exit 1
+		;;
+
+esac
